4e00ac48ebf905999fd4f7b1d3c454c97bd800cf
Mohammad Rizqy Akmaluddin <rizqy.akmaluddin@gmail.com>
2026-01-05 02:36:10 +0700

    Commit baru gabungan dari 4 sebelumnya

--- database/seeders/AccessSeeder.php
+++ database/seeders/AccessSeeder.php
@@ -9,7 +9,32 @@ class AccessSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('access_management')->insert([
+        // DB::table('access_management')->insert([
+        //     ['company_id' => 'C001', 'user_id' => 'U061'],
+        //     ['company_id' => 'C001', 'user_id' => 'U004'],
+        //     ['company_id' => 'C002', 'user_id' => 'U063'],
+        //     ['company_id' => 'C002', 'user_id' => 'U004'],
+        //     ['company_id' => 'C003', 'user_id' => 'U063'],
+        //     ['company_id' => 'C003', 'user_id' => 'U004'],
+        //     ['company_id' => 'C004', 'user_id' => 'U063'],
+        //     ['company_id' => 'C004', 'user_id' => 'U004'],
+        //     ['company_id' => 'C005', 'user_id' => 'U062'],
+        //     ['company_id' => 'C005', 'user_id' => 'U004'],
+        //     ['company_id' => 'C006', 'user_id' => 'U102'],
+        //     ['company_id' => 'C006', 'user_id' => 'U004'],
+        //     ['company_id' => 'C007', 'user_id' => 'U101'],
+        //     ['company_id' => 'C007', 'user_id' => 'U004'],
+        //     ['company_id' => 'C008', 'user_id' => 'U102'],
+        //     ['company_id' => 'C008', 'user_id' => 'U004'],
+        //     ['company_id' => 'C009', 'user_id' => 'U003'],
+        //     ['company_id' => 'C010', 'user_id' => 'U004'],
+        //     ['company_id' => 'C011', 'user_id' => 'U006'],
+        //     ['company_id' => 'C012', 'user_id' => 'U004'],
+        //     ['company_id' => 'C012', 'user_id' => 'U017'],
+        //     ['company_id' => 'C013', 'user_id' => 'U004'],
+        // ]);
+
+        $accessList = [
             ['company_id' => 'C001', 'user_id' => 'U061'],
             ['company_id' => 'C001', 'user_id' => 'U004'],
             ['company_id' => 'C002', 'user_id' => 'U063'],
@@ -32,6 +57,16 @@ class AccessSeeder extends Seeder
             ['company_id' => 'C012', 'user_id' => 'U004'],
             ['company_id' => 'C012', 'user_id' => 'U017'],
             ['company_id' => 'C013', 'user_id' => 'U004'],
-        ]);
+        ];
+
+        foreach ($accessList as $access) {
+            DB::table('access_management')->updateOrInsert(
+                [
+                    'company_id' => $access['company_id'],
+                    'user_id'    => $access['user_id'], // primary key composite
+                ],
+                [] // tidak ada field lain yang perlu update
+            );
+        }
     }
 }
