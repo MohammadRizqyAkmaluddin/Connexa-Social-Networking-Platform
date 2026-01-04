9cfa37e00046d9bd65e61812a9216e96e943a02b
Mohammad Rizqy Akmaluddin <rizqy.akmaluddin@gmail.com>
2026-01-05 01:13:15 +0700

    Commit baru gabungan dari 4 sebelumnya

--- database/seeders/EmploymentSeeder.php
+++ database/seeders/EmploymentSeeder.php
@@ -7,26 +7,28 @@ use Illuminate\Support\Facades\DB;
 
 class EmploymentSeeder extends Seeder
 {
-    
+
     public function run()
-    {
-        DB::table('employment')->insert([
-            [
-                'employment_id'   => 'FT',
-                'employment_type' => 'Full-time',
-            ],
-            [
-                'employment_id'   => 'PT',
-                'employment_type' => 'Part-time',
-            ],
-            [
-                'employment_id'   => 'CO',
-                'employment_type' => 'Contract',
-            ],
-            [
-                'employment_id'   => 'IN',
-                'employment_type' => 'Internship',
-            ],
-        ]);
-    }
+{
+    DB::table('employment')->updateOrInsert(
+        ['employment_id' => 'FT'],
+        ['employment_type' => 'Full-time']
+    );
+
+    DB::table('employment')->updateOrInsert(
+        ['employment_id' => 'PT'],
+        ['employment_type' => 'Part-time']
+    );
+
+    DB::table('employment')->updateOrInsert(
+        ['employment_id' => 'CO'],
+        ['employment_type' => 'Contract']
+    );
+
+    DB::table('employment')->updateOrInsert(
+        ['employment_id' => 'IN'],
+        ['employment_type' => 'Internship']
+    );
+}
+
 }
