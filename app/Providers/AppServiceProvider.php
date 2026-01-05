8cae529f70148500339b66e82f8b11b2f3ffcde7
Mohammad Rizqy Akmaluddin <rizqy.akmaluddin@gmail.com>
2026-01-05 18:44:30 +0700

    Commit baru gabungan dari 4 sebelumnya

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
          Carbon::macro('shortDiff', function () {
             $now = now();
 
